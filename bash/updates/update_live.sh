13d77374c07a1a2d7fd01742691de0218d39cbe2
Nick Veenhof <nick.veenhof@gmail.com>
2018-07-07 22:04:42 +0200

    Making sure that the password is static

--- bash/updates/update_live.sh
+++ bash/updates/update_live.sh
@@ -39,7 +39,7 @@ wget -P docroot/libraries/jquery-ui-slider-pips https://raw.githubusercontent.co
 wget -P docroot/libraries/jquery-ui-slider-pips https://raw.githubusercontent.com/simeydotme/jQuery-ui-Slider-Pips/v1.11.3/dist/jquery-ui-slider-pips.min.js
 cp -R docroot/core/profiles/demo_umami/themes/umami docroot/themes/contrib/
 cp -R docroot/core/profiles/demo_umami/modules/demo_umami_content docroot/modules/contrib/
-${DRUSH} site-install --db-url=sqlite://../.ht.sqlite --verbose config_installer config_installer_sync_configure_form.sync_directory=../config/sync/ --yes
+${DRUSH} site-install --account-name=drupalsearch --account-pass=drupalsearch --db-url=sqlite://../.ht.sqlite --verbose config_installer config_installer_sync_configure_form.sync_directory=../config/sync/ --yes
 ${DRUSH} ev '\Drupal::classResolver()->getInstanceFromDefinition(Drupal\demo_umami_content\InstallHelper::class)->importContent();'
 ${DRUSH} search-api:reset-tracker
 ${DRUSH} search-api:index
