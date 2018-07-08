100435d99769537cc6bca01c09ea1a4203959f03
Nick Veenhof <nick.veenhof@gmail.com>
2018-07-08 21:58:14 +0200

    Making sure you can reinstall the site as well

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,14 +2,17 @@
 
 composer install
 mkdir docroot/themes/contrib
+rm -rf docroot/libraries/jquery-ui-slider-pips
 mkdir docroot/libraries/jquery-ui-slider-pips
 wget -P docroot/libraries/jquery-ui-slider-pips https://raw.githubusercontent.com/simeydotme/jQuery-ui-Slider-Pips/v1.11.3/dist/jquery-ui-slider-pips.min.css
 wget -P docroot/libraries/jquery-ui-slider-pips https://raw.githubusercontent.com/simeydotme/jQuery-ui-Slider-Pips/v1.11.3/dist/jquery-ui-slider-pips.min.js
 cp -R docroot/core/profiles/demo_umami/themes/umami docroot/themes/contrib/
 cp -R docroot/core/profiles/demo_umami/modules/demo_umami_content docroot/modules/contrib/
-drush site-install --db-url=sqlite://../.ht.sqlite --verbose config_installer config_installer_sync_configure_form.sync_directory=../config/sync/ --yes
-drush en demo_umami_content
-drush en demo_umami_search_content
+rm .ht.sqlite || :
+sudo rm -rf web/sites/default/settings.php
+drush site-install --db-url=sqlite://.ht.sqlite config_installer config_installer_sync_configure_form.sync_directory=../config/sync/ --yes
+drush ev '\Drupal::classResolver()->getInstanceFromDefinition(Drupal\demo_umami_content\InstallHelper::class)->importContent();'
+drush ev '\Drupal::classResolver()->getInstanceFromDefinition(Drupal\demo_umami_search_content\InstallHelper::class)->importContent();'
 drush search-api:reset-tracker
 drush search-api:index
 drush cr
