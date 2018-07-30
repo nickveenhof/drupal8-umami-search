997af3a994ffe83d2c1d1bd09cc2c8601889356d
Nick Wilde <nick@briarmoon.ca>
2018-07-30 08:45:45 -0700

    Add Asset Packagist Add package for Facet's slider pips

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,10 +2,6 @@
 
 composer install
 mkdir docroot/themes/contrib
-rm -rf docroot/libraries/jquery-ui-slider-pips
-mkdir docroot/libraries/jquery-ui-slider-pips
-wget -P docroot/libraries/jquery-ui-slider-pips https://raw.githubusercontent.com/simeydotme/jQuery-ui-Slider-Pips/v1.11.3/dist/jquery-ui-slider-pips.min.css
-wget -P docroot/libraries/jquery-ui-slider-pips https://raw.githubusercontent.com/simeydotme/jQuery-ui-Slider-Pips/v1.11.3/dist/jquery-ui-slider-pips.min.js
 cp -R docroot/core/profiles/demo_umami/themes/umami docroot/themes/contrib/
 cp -R docroot/core/profiles/demo_umami/modules/demo_umami_content docroot/modules/contrib/
 rm .ht.sqlite || :
