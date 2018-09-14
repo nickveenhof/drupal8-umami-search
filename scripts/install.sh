a1e372d5de6f52485675a9423a4fa1c66a183480
Ricardo Amaro <ricardoamaro@users.noreply.github.com>
2018-09-14 08:55:42 +0100

    Make sure creating libraries sub-directory
    On the code sprint this failed because libraries sub-directory didn't exist

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,7 +3,7 @@
 composer install
 mkdir docroot/themes/contrib
 rm -rf docroot/libraries/jquery-ui-slider-pips
-mkdir docroot/libraries/jquery-ui-slider-pips
+mkdir -p docroot/libraries/jquery-ui-slider-pips
 wget -P docroot/libraries/jquery-ui-slider-pips https://raw.githubusercontent.com/simeydotme/jQuery-ui-Slider-Pips/v1.11.3/dist/jquery-ui-slider-pips.min.css
 wget -P docroot/libraries/jquery-ui-slider-pips https://raw.githubusercontent.com/simeydotme/jQuery-ui-Slider-Pips/v1.11.3/dist/jquery-ui-slider-pips.min.js
 cp -R docroot/core/profiles/demo_umami/themes/umami docroot/themes/contrib/
