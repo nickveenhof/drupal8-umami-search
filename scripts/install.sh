0281da2317958e6d6f361b741ce514565acd458d
sgurlt <gurlt@brightsolutions.de>
2018-09-14 10:07:37 +0200

    Use the local instead of the global Drush binary.

--- scripts/install.sh
+++ scripts/install.sh
@@ -10,9 +10,9 @@ cp -R docroot/core/profiles/demo_umami/themes/umami docroot/themes/contrib/
 cp -R docroot/core/profiles/demo_umami/modules/demo_umami_content docroot/modules/contrib/
 rm .ht.sqlite || :
 sudo rm -rf web/sites/default/settings.php
-drush site-install --db-url=sqlite://.ht.sqlite config_installer config_installer_sync_configure_form.sync_directory=../config/sync/ --yes
-drush ev '\Drupal::classResolver()->getInstanceFromDefinition(Drupal\demo_umami_content\InstallHelper::class)->importContent();'
-drush ev '\Drupal::classResolver()->getInstanceFromDefinition(Drupal\demo_umami_search_content\InstallHelper::class)->importContent();'
-drush search-api:reset-tracker
-drush search-api:index
-drush cr
+vendor/bin/drush site-install --db-url=sqlite://.ht.sqlite config_installer config_installer_sync_configure_form.sync_directory=../config/sync/ --yes
+vendor/bin/drush ev '\Drupal::classResolver()->getInstanceFromDefinition(Drupal\demo_umami_content\InstallHelper::class)->importContent();'
+vendor/bin/drush ev '\Drupal::classResolver()->getInstanceFromDefinition(Drupal\demo_umami_search_content\InstallHelper::class)->importContent();'
+vendor/bin/drush search-api:reset-tracker
+vendor/bin/drush search-api:index
+vendor/bin/drush cr
