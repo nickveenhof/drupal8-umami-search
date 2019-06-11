bb1be2a82d92626fba40ec7ec07956d26cb68ffb
Nick Veenhof <nick.veenhof@gmail.com>
2019-06-11 18:18:39 +0300

    Updating to Drupal 8.8

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 composer install
-mkdir docroot/themes/contrib
 cp -R docroot/core/profiles/demo_umami/themes/umami docroot/themes/contrib/
 cp -R docroot/core/profiles/demo_umami/modules/demo_umami_content docroot/modules/contrib/
 rm .ht.sqlite || :
-sudo rm -rf web/sites/default/settings.php
+sudo rm -rf docroot/sites/default/settings.php
+chmod +w docroot/sites/default
 vendor/bin/drush site-install --db-url=sqlite://.ht.sqlite config_installer config_installer_sync_configure_form.sync_directory=../config/sync/ --yes
 vendor/bin/drush ev '\Drupal::classResolver()->getInstanceFromDefinition(Drupal\demo_umami_content\InstallHelper::class)->importContent();'
 vendor/bin/drush ev '\Drupal::classResolver()->getInstanceFromDefinition(Drupal\demo_umami_search_content\InstallHelper::class)->importContent();'
