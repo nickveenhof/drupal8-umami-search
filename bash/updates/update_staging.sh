20970f9f71196c98dfc50d2314fcd43267e0e625
Nick Veenhof <nick.veenhof@gmail.com>
2019-06-11 18:36:18 +0300

    Updating update scripts for the live deployment

--- bash/updates/update_staging.sh
+++ bash/updates/update_staging.sh
@@ -27,16 +27,16 @@ DOCROOT="$SCRIPT_DIR/../../docroot"
 cd $SCRIPT_DIR/../../
 
 # D8
-rm ../.ht.sqlite
+
 composer install
-mkdir docroot/themes/contrib
-mkdir docroot/libraries/jquery-ui-slider-pips
-wget -P docroot/libraries/jquery-ui-slider-pips https://raw.githubusercontent.com/simeydotme/jQuery-ui-Slider-Pips/v1.11.3/dist/jquery-ui-slider-pips.min.css
-wget -P docroot/libraries/jquery-ui-slider-pips https://raw.githubusercontent.com/simeydotme/jQuery-ui-Slider-Pips/v1.11.3/dist/jquery-ui-slider-pips.min.js
 cp -R docroot/core/profiles/demo_umami/themes/umami docroot/themes/contrib/
 cp -R docroot/core/profiles/demo_umami/modules/demo_umami_content docroot/modules/contrib/
-${DRUSH} site-install --db-url=sqlite://../.ht.sqlite --verbose config_installer config_installer_sync_configure_form.sync_directory=../config/sync/ --yes
-${DRUSH} ev '\Drupal::classResolver()->getInstanceFromDefinition(Drupal\demo_umami_content\InstallHelper::class)->importContent();'
-${DRUSH} search-api:reset-tracker
-${DRUSH} search-api:index
-${DRUSH} cr
+rm .ht.sqlite || :
+rm -rf docroot/sites/default/settings.php || :
+chmod +w docroot/sites/default
+vendor/bin/drush site-install --account-name=drupalsearch --account-pass=drupalsearch --db-url=sqlite://.ht.sqlite config_installer config_installer_sync_configure_form.sync_directory=../config/sync/ --yes
+vendor/bin/drush ev '\Drupal::classResolver()->getInstanceFromDefinition(Drupal\demo_umami_content\InstallHelper::class)->importContent();'
+vendor/bin/drush ev '\Drupal::classResolver()->getInstanceFromDefinition(Drupal\demo_umami_search_content\InstallHelper::class)->importContent();'
+vendor/bin/drush search-api:reset-tracker
+vendor/bin/drush search-api:index
+vendor/bin/drush cr
