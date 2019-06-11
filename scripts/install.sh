fd12aa4c507ea4ab28efb6386134ce9632c0e66a
Nick Veenhof <nick.veenhof@gmail.com>
2019-06-11 18:30:45 +0300

    #28 - Fixing username/password issue

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,7 +6,7 @@ cp -R docroot/core/profiles/demo_umami/modules/demo_umami_content docroot/module
 rm .ht.sqlite || :
 sudo rm -rf docroot/sites/default/settings.php
 chmod +w docroot/sites/default
-vendor/bin/drush site-install --db-url=sqlite://.ht.sqlite config_installer config_installer_sync_configure_form.sync_directory=../config/sync/ --yes
+vendor/bin/drush site-install --account-name=drupalsearch --account-pass=drupalsearch --db-url=sqlite://.ht.sqlite config_installer config_installer_sync_configure_form.sync_directory=../config/sync/ --yes
 vendor/bin/drush ev '\Drupal::classResolver()->getInstanceFromDefinition(Drupal\demo_umami_content\InstallHelper::class)->importContent();'
 vendor/bin/drush ev '\Drupal::classResolver()->getInstanceFromDefinition(Drupal\demo_umami_search_content\InstallHelper::class)->importContent();'
 vendor/bin/drush search-api:reset-tracker
